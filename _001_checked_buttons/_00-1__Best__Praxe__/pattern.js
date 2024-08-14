7295bdf39ad1b4bea7cd7d26994de86a4fa58115
AndriiKot <a_kocyuba@ukr.net>
2024-08-14 11:16:28 +0200

    VanillaJS  'VanillaJS__Cooks__' 'Checked Menu Patern' ! added new Best praxe with DOM modif !

---
+++ _001_checked_buttons/_00-1__Best__Praxe__/pattern.js
@@ -0,0 +1,29 @@
+﻿const menu = ["Menu", "Home", "About", "Contact"];
+let activeBtn;
+
+// Создаем фрагмент документа
+const fragment = document.createDocumentFragment();
+
+menu.forEach((item) => {
+  const btn = document.createElement("button");
+  btn.className = "menu-button";
+  btn.textContent = item;
+  fragment.appendChild(btn); // Добавляем кнопку во фрагмент
+});
+
+// Добавляем все кнопки сразу в документ
+document.body.appendChild(fragment);
+
+// Обработчик событий для делегирования
+document.body.addEventListener("click", (event) => {
+  if (event.target.className === "menu-button") {
+    const el = event.target;
+    if (activeBtn !== el) {
+      if (activeBtn) {
+        activeBtn.classList.remove("active"); // Удаляем класс у предыдущей кнопки
+      }
+      el.classList.add("active"); // Добавляем класс к нажимаемой кнопке
+      activeBtn = el; // Обновляем активную кнопку
+    }
+  }
+});
---
+++ _001_checked_buttons/_01-1__pattern__original/README.md
@@ -0,0 +1,19 @@
+﻿
+#### ! For the code to work correctly, use 'live server' in Visual Studio Code !
+
+```js
+const menu = ["Menu", "Home", "About", "Contact"];
+let activeBtn;
+
+for (const item of menu) {
+  const btn = document.createElement("button");
+  btn.textContent = item;
+  document.body.append(btn);
+
+  btn.onclick = () => {
+    if (activeBtn) activeBtn.classList.remove("active");  // delete the old active button
+    btn.classList.add("active");
+    activeBtn = btn;
+  };
+}
+```
