9811844066a1e719d59d2f75ae699e12b56cfd57
AndriiKot <a_kocyuba@ukr.net>
2024-08-17 13:04:10 +0200

    VanillaJS  'VanillaJS__Cooks__' 'Checked Menu Patern' ! update .GitManager v2_0_0 testing 5 !

--- .GitManager/update_childere-readme.rb
+++ .GitManager/update_childere-readme.rb
@@ -16,11 +16,11 @@ subfolders.each do |folder|
     if child_filter && !childname.match?(/_00-0__Best__Praxe__/)
     content = File.read(child.join("pattern.js"))
       File.open(child.join("README.md"), "w+") do |f|
-        f.puts  "#### ! For the code to work correctly, use 'live server' in Visual Studio Code !\n\n"
-        f.puts "```js\n"
+        f.puts  "#### ! For the code to work correctly, use 'live server' in Visual Studio Code !"
+        f.puts "[![Pattern Checked Buttons](https://github.com/AndriiKot/VanillaJS__Cooks/blob/main/#{link}/__demo__/__v1_0_0__.png)](https://github.com/AndriiKot/VanillaJS__Cooks/blob/main/#{link}/_00-0__Best__Praxe__)"
+        f.puts "```js"
         f.puts content
-        f.puts "```\n"
-        f.puts "[![Pattern Checked Buttons](https://github.com/AndriiKot/VanillaJS__Cooks/blob/main/#{link}/__demo__/__v1_0_0__.png)](https://github.com/AndriiKot/VanillaJS__Cooks/blob/main/#{link}/_00-0__Best__Praxe__)\n\n"
+        f.puts "```"
       end
       versions << child
     end
@@ -33,13 +33,12 @@ subfolders.each do |folder|
     pattern_name = folder.basename.to_s
     pattern_name = "Pattern " + pattern_name.split("_").reject{|x| x.to_i > 0 || x == ''}.map(&:capitalize).join(" ")
     File.open(folder.join("README.md"), "w+") do |f|
-      f.puts("# #{pattern_name}\n")
-      f.puts "[![#{pattern_name}](https://github.com/AndriiKot/VanillaJS__Cooks/blob/main/#{link}/__demo__/__v1_0_0__.png)](https://github.com/AndriiKot/VanillaJS__Cooks/blob/main/#{link}/_00-0__Best__Praxe__)\n"
-      f.puts("\n")
-      f.puts("### Best Praxe:\n")
-      f.puts("```js\n")
+      f.puts("# #{pattern_name}")
+      f.puts "[![#{pattern_name}](https://github.com/AndriiKot/VanillaJS__Cooks/blob/main/#{link}/__demo__/__v1_0_0__.png)](https://github.com/AndriiKot/VanillaJS__Cooks/blob/main/#{link}/_00-0__Best__Praxe__)"
+      f.puts("### Best Praxe: ")
+      f.puts("```js")
       f.puts(File.read(folder.join("_00-0__Best__Praxe__").join("pattern.js")))
-      f.puts("```\n")
+      f.puts("```")
     end
   end
 end
