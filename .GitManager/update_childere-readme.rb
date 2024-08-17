98e34342683213436a6aefb947a0caef256980f8
AndriiKot <a_kocyuba@ukr.net>
2024-08-17 12:00:53 +0200

    VanillaJS  'VanillaJS__Cooks__' 'Checked Menu Patern' ! update .GitManager v1_0_0 !

--- .GitManager/update_childere-readme.rb
+++ .GitManager/update_childere-readme.rb
@@ -6,17 +6,14 @@ parent_folder = current_folder.parent
 
 subfolders = parent_folder.children.select(&:directory?)
 
-versions = []
-readme_bests_praxe = []
 
 subfolders.each do |folder|
   link = folder.basename.to_s if folder.basename.to_s.match?(/^_(\d{1})/)
-  folder.children.each do |child|
+  folder.children.each_with_index do |child, i|
     childname = child.basename.to_s
     child_filter = childname.match?(/^_(\d{1})/)
     if child_filter
     content = File.read(child.join("pattern.js"))
-      versions << child
       File.open(child.join("README.md"), "w+") do |f|
         f.puts  "#### ! For the code to work correctly, use 'live server' in Visual Studio Code !\n\n"
         f.puts "```js\n"
@@ -25,26 +22,6 @@ subfolders.each do |folder|
         f.puts "[![Pattern Checked Buttons](https://github.com/AndriiKot/VanillaJS__Cooks/blob/main/#{link}/__demo__/__v1_0_0__.png)](https://github.com/AndriiKot/VanillaJS__Cooks/blob/main/#{link}/_00-0__Best__Praxe__)\n\n"
       end
     end
-    if childname == "README.md"
-      readme_bests_praxe << child
-    end
   end
 end
-
-last_version = versions.sort![-1]
-
-source_path = last_version
-best_praxe_folder = source_path.parent.join('_00-0__Best__Praxe__')
-
-FileUtils.mkdir_p(best_praxe_folder) unless best_praxe_folder.exist?
-FileUtils.cp_r("#{source_path}/.", best_praxe_folder)
-
-readme_bests_praxe.each do |readme|
-  File.open(readme, "a+") do |f|
-    f.puts "```js\n\n"
-    f.puts("#{File.read(best_praxe_folder.join("pattern.js"))}\n" )
-    f.puts "```\n\n"
-  end
-end
-
 
